79e07b696697f79de94fcff744363d305c9b1e95
Tom White <tom.e.white@gmail.com>
2024-08-06 15:45:15 +0100

    Add a test

--- cubed_xarray/tests/test_wrapping.py
+++ cubed_xarray/tests/test_wrapping.py
@@ -1,8 +1,23 @@
 import xarray as xr
+from xarray.tests import assert_allclose, create_test_data
+
 from xarray.namedarray.parallelcompat import list_chunkmanagers
+import pytest
 import cubed
+from cubed.runtime.create import create_executor
 
 from cubed_xarray.cubedmanager import CubedManager
+
+
+EXECUTORS = [create_executor("single-threaded"), create_executor("processes")]
+
+@pytest.fixture(
+    scope="module",
+    params=EXECUTORS,
+    ids=[executor.name for executor in EXECUTORS],
+)
+def executor(request):
+    return request.param
 
 
 class TestDiscoverCubedManager:
@@ -20,3 +35,19 @@ class TestDiscoverCubedManager:
     # TODO test cubed is default when dask not installed
 
     # TODO test dask is default over cubed when both installed
+
+
+def test_to_zarr(tmpdir, executor):
+    spec = cubed.Spec(allowed_mem="200MB", executor=executor)
+
+    original = create_test_data().chunk(chunked_array_type="cubed", from_array_kwargs={'spec': spec})
+
+    filename = tmpdir / "out.zarr"
+    original.to_zarr(filename)
+
+    with xr.open_dataset(
+        filename, chunks="auto", engine="zarr", chunked_array_type="cubed", from_array_kwargs={'spec': spec}
+    ) as restored:
+        assert isinstance(restored.var1.data, cubed.Array)
+        computed = restored.compute()
+        assert_allclose(original, computed)
